02631ef0dff637cee438bf8c0d6a7c820b9917ef
vladZakiev <90211959+vladZakiev@users.noreply.github.com>
2022-05-08 22:49:50 +0500

    tabs with data

---
+++ script.js
@@ -0,0 +1,17 @@
+let tabNav = [...document.querySelectorAll('.tabs-navigation .tab')];
+let tabContent = [...document.querySelectorAll('.tab-content div')];;
+console.log(tabContent);
+tabNav.forEach(element=> {
+    element.addEventListener('click',(element)=>{
+        tabNav.forEach(element=>{
+            element.classList.remove('active')
+        })
+        element.currentTarget.classList.add('active');
+
+        tabContent.forEach(div=>{
+            div.style.display='none'
+        })
+        document.querySelector(element.currentTarget.dataset.content).style.display='block';
+    })
+})
+
